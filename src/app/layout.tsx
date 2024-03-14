a83b56ea4dad3e344f6aaf0b2019f8abed8464e3
hoapng <giahoa01giathieu@gmail.com>
2024-03-14 10:46:26 +0700

    session client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import ThemeRegistry from "@/components/theme-registry/theme.registry";
 import AppHeader from "../components/header/app.header";
 import AppFooter from "@/components/footer/app.footer";
+import NextAuthWrapper from "@/lib/next.auth.provider";
 
 export default function RootLayout({
   children,
@@ -11,9 +12,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppHeader />
-          {children}
-          <AppFooter />
+          <NextAuthWrapper>
+            <AppHeader />
+            {children}
+            <AppFooter />
+          </NextAuthWrapper>
         </ThemeRegistry>
       </body>
     </html>
--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -20,6 +20,7 @@ import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 //styled-component
 const Search = styled("div")(({ theme }) => ({
@@ -64,6 +65,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function AppHeader() {
+  const { data: session } = useSession();
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
@@ -219,10 +222,18 @@ export default function AppHeader() {
                 },
               }}
             >
-              <Link href={"/playlist"}>Playlists</Link>
-              <Link href={"/likes"}>Likes</Link>
-              <Link href={""}>Upload</Link>
-              <Avatar onClick={handleProfileMenuOpen}>GH</Avatar>
+              {session ? (
+                <>
+                  <Link href={"/playlist"}>Playlists</Link>
+                  <Link href={"/likes"}>Likes</Link>
+                  <Link href={""}>Upload</Link>
+                  <Avatar onClick={handleProfileMenuOpen}>GH</Avatar>
+                </>
+              ) : (
+                <>
+                  <Link href={"/api/auth/signin"}>Login</Link>
+                </>
+              )}
             </Box>
             <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
