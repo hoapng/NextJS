9e8d102f0b5f2e682a7f4bdf66f467a1e1da17e3
hoapng <giahoa01giathieu@gmail.com>
2024-03-14 10:53:18 +0700

    session server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+
   const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
